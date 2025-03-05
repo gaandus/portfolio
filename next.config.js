92b93f49b964d576215d9087ee351347fda479aa
Daniel Anderson <danielthomasanderson@gmail.com>
2025-03-05 16:48:48 -0600

    Fixed Next.js App Router CSS issue

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  output: 'standalone', // Ensure static files are served
+  assetPrefix: '/',
 };
 
 module.exports = nextConfig;
